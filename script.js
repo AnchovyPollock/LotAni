abbfac003b85cd40aa3e132c41c013e262f011cf
AnchovyPollock <126333134+AnchovyPollock@users.noreply.github.com>
2026-07-17 01:07:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
             // Set marker if available
             try {
                 if (typeof dotLottieInstance.setMarker === 'function') {
-                    dotLottieInstance.setMarker('Wizard');
+                    dotLottieInstance.setMarker('Orbit'); //Wizard
                     // dotLottieInstance.setSegment(250,400);                    
                     console.log('✅ Marker / segment set');
                     const markers = dotLottieInstance.markers(1);
